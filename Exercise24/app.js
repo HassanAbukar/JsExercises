dc7da689a2d5d2380ebeeea75fe70d16b6c947a8
HassanAbukar <xasaani.abukar@gmail.com>
2026-05-31 21:58:55 -0700

    updated exercise24

--- Exercise24/app.js
+++ Exercise24/app.js
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-// Use object destructuring to extract the make and the model properties 
-// from the object {make:"Tayota", model : "Corrola", year : 2020}
 
-const Car = {make:"Tayota", model : "Corrola", year : 2020};
-
-const [first, second , third] = Car
-
-console.log(first) // red
-console.log(third) // blue
-=======
 // create function CalculateArea that takes width and height as parameter and returns the area.
 // if no height is provided, it should default to width (calculating the area of the square)
 
@@ -20,4 +10,4 @@ function calculateArea(width, height = width) {
 console.log(calculateArea(20, 5))  // 100  
 // Square (height la siin — default = width)
 console.log(calculateArea(10))      // 100 
->>>>>>> d237eb6c50add6b75b23002430472c8b8598c998
+
